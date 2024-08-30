bf95d4403bd545fcf479d36a1ad4b4883d2fce7f
HB_the_Pencil <118207101+HB-the-Pencil@users.noreply.github.com>
2024-08-30 09:10:44 -0400

    Add TIY 13 and practice files

---
+++ barcalow_tiy_2-13_sep=_and_end=.py
@@ -0,0 +1,16 @@
+# Goal: print the string "The_rain_in_Spain**is**mainly**on_the_plain!!!"
+
+print("The","rain","in","Spain",sep="_",end="**")
+print("is","mainly",sep="**",end="**")
+print("on","the","plain",sep="_",end="!!!")
+
+# Paste the output here. This is my basic error checker...
+# I can't figure out how to read the previous line so you paste it in manually :P
+'''
+check = input()
+
+if check == "The_rain_in_Spain**is**mainly**on_the_plain!!!":
+    print("you did it!")
+else:
+    print("Something's wrong with your prints...")
+'''
