9ee8fde4883e0ed7264a8b75660b303d8d07c56c
HB_the_Pencil <118207101+HB-the-Pencil@users.noreply.github.com>
2024-08-30 08:48:21 -0400

    Respond to the Zen of Python

--- barcalow_tiy_2-12_zen_of_python.py
+++ barcalow_tiy_2-12_zen_of_python.py
@@ -1,2 +1,43 @@
 # It's so cool that you can do this.
 import this
+
+# The Zen of Python, by Tim Peters
+#
+# Beautiful is better than ugly.
+  # Make sure your code looks nice so that people can read it.
+# Explicit is better than implicit.
+  # Define your variables and functions clearly. Don't make people or programs assume anything.
+# Simple is better than complex.
+  # Complex code takes longer to run. Use faster methods whenever possible.
+# Complex is better than complicated.
+  # Something that takes longer to run is better than an unintelligible formula.
+# Flat is better than nested.
+  # Don't nest loops so far that they're a whole screen to the right. If your code is that complicated, it should be rewritten.
+# Sparse is better than dense.
+  # Space your code out so that it's readable. Don't cramEverythingIntoOneSingleLine like that. Put whitespace between lines.
+# Readability counts.
+  # If you can't understand your code, how can you fix it? How can anyone else understand it, either?
+# Special cases aren't special enough to break the rules.
+  # I'm not entirely sure what a special case is, but the point here is to stick to the formula, even if it's a bit redundant.
+# Although practicality beats purity.
+  # Use coding to your advantage. Don't follow the letter of the law, but the spirit.
+# Errors should never pass silently.
+  # If something breaks, it should be obvious that it broke.
+# Unless explicitly silenced.
+  # "It's not a bug, it's a feature!"
+# In the face of ambiguity, refuse the temptation to guess.
+  # Your code should be understandable. If it isn't, make it understandable. Don't make people guess your intent.
+# There should be one-- and preferably only one --obvious way to do it.
+  # Though there may be an obvious way, it may not be the only way. Just the most convenient or common.
+# Although that way may not be obvious at first unless you're Dutch.
+  # this line's just funny :D
+# Now is better than never.
+  # Don't put off work because you feel like you aren't ready.
+# Although never is often better than *right* now.
+  # At the same time, you need to be prepared for what you're trying to do.
+# If the implementation is hard to explain, it's a bad idea.
+  # If you can't understand your idea, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+  # But if you can, and everyone else understands it, it may be worth a try!
+# Namespaces are one honking great idea -- let's do more of those!
+  # I'm not sure what namespaces are. But it's clear that if an idea is good, let's do it more often!
